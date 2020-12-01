fbce4868da12063e9fab3cceeafdfabc99113ca5
Carlton627 <carlton_rodrigues21@outlook.com>
2020-12-02 00:18:10 +0530

    extension checking version 1 implemented

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use std::error::Error;
+
+pub fn extension_checker(env_args: &Vec<String>) -> Result<String, &'static str> {
+    let filename_extension: Vec<&str> = env_args[1].split('.').collect();
+    let filename = filename_extension[0].clone();
+    let extension = filename_extension[1].clone();
+
+    if extension == "fer" {
+        Ok(String::from(filename) + "." + extension)
+    } else {
+        Err("Please use extension .fer for program files")
+    }
+}
+
+pub fn run(file: String) -> Result<(), Box<dyn Error>> {
+    let program = fs::read_to_string(file).expect("Some problem occurred");
+    println!("{}", program);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
+use ferment::extension_checker;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = extension_checker(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    println!("{}", file);
+
+    if let Err(e) = ferment::run(file) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
