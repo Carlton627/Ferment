507437abee9b3f0c75a214d530e2f0b046fc4bf1
Carlton627 <carlton_rodrigues21@outlook.com>
2021-10-07 17:02:33 +0530

    error handling module stage 1 of 3 complete

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,40 @@ use std::fs;
 
 use modules::tokenizer;
 
-// TODO: Implement Error handling structure
+/////////////////////////////// CONSTANTS ///////////////////////////////////
+/// TODO: give constants a global scope across the crate
+
+const NFNE: &str = "NFNE";
+const INEXTE: &str = "INEXTE";
+
+/////////////////////////////////////////////////////////////////////////////
+
+// TODO: ErrorMessages needs to be kept separately in a file to be used across the crate
+
+#[derive(Debug)]
+pub struct ErrorMessages {
+    pub error_type: String,
+}
+
+impl ErrorMessages {
+    pub fn throw_error(&self) -> &'static str {
+        if self.error_type == String::from(NFNE) {
+            return self.no_file_name_error();
+        } else if self.error_type == String::from(INEXTE) {
+            return self.invalid_extension_error();
+        } else {
+            "a"
+        }
+    }
+
+    fn no_file_name_error(&self) -> &'static str {
+        "NoFileNameError"
+    }
+
+    fn invalid_extension_error(&self) -> &'static str {
+        "InvalidExtensionError"
+    }
+}
 
 #[derive(Debug)]
 pub struct Config {
@@ -16,13 +49,19 @@ pub struct Config {
 impl Config {
     pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 2 {
-            return Err("NoFileNameError");
+            let get_error_message = ErrorMessages {
+                error_type: String::from(NFNE),
+            };
+            return Err(get_error_message.throw_error());
         }
 
         let filename_extension: Vec<&str> = args[1].split('.').collect();
 
         if filename_extension.len() < 2 {
-            return Err("InvalidExtensionError");
+            let get_error_message = ErrorMessages {
+                error_type: String::from(INEXTE),
+            };
+            return Err(get_error_message.throw_error());
         }
 
         let filename = filename_extension[0].clone();
@@ -32,7 +71,10 @@ impl Config {
                 filename: String::from(filename) + "." + extension,
             })
         } else {
-            Err("InvalidExtensionError")
+            let get_error_message = ErrorMessages {
+                error_type: String::from(INEXTE),
+            };
+            Err(get_error_message.throw_error())
         }
     }
 
@@ -43,7 +85,7 @@ impl Config {
         for token in tokens {
             println!("{:?}", token.token_type);
             println!("{:?}", token.value);
-            println!("")
+            println!("");
         }
         Ok(())
     }
--- src/modules/tokenizer.rs
+++ src/modules/tokenizer.rs
@@ -3,7 +3,7 @@ use crate::models::token;
 extern crate regex;
 use regex::Regex;
 
-// TODO: basic syntax analysis 
+// TODO: basic syntax analysis
 // TODO: re-implement the lexer to include the symbol table
 
 pub fn lexer(program: String) -> Result<Vec<token::Token>, &'static str> {
@@ -13,46 +13,35 @@ pub fn lexer(program: String) -> Result<Vec<token::Token>, &'static str> {
     let alphabets = Regex::new(r"[a-zA-Z]").unwrap();
     let operators = Regex::new(r"[=+-/*]").unwrap();
     let _whitespace = Regex::new(r"[\s]").unwrap();
-         
     for token in program.chars() {
         if numbers.is_match(&token.to_string()) {
-
             tokens.push(token::Token {
                 token_type: token::TokenType::Number,
-                value: token::TokenDataType::Number(token.to_string().parse::<i32>().unwrap())
+                value: token::TokenDataType::Number(token.to_string().parse::<i32>().unwrap()),
             });
-
         } else if alphabets.is_match(&token.to_string()) {
-            
             tokens.push(token::Token {
                 token_type: token::TokenType::Character,
-                value: token::TokenDataType::Word(token.to_string())
+                value: token::TokenDataType::Word(token.to_string()),
             });
-
         } else if operators.is_match(&token.to_string()) {
-
             tokens.push(token::Token {
                 token_type: token::TokenType::Operator,
-                value: token::TokenDataType::Character(token)
+                value: token::TokenDataType::Character(token),
             });
-
         } else {
-
             tokens.push(token::Token {
                 token_type: match token {
                     '(' => token::TokenType::OpenParenthesis,
                     ')' => token::TokenType::CloseParenthesis,
                     '{' => token::TokenType::BeginBracket,
                     '}' => token::TokenType::EndBracket,
-                    _  => token::TokenType::SpecialChar,
+                    _ => token::TokenType::SpecialChar,
                 },
-                value: token::TokenDataType::Character(token)
+                value: token::TokenDataType::Character(token),
             });
-
         }
     }
-    
     Ok(tokens)
     // Err("cant lex code")
 }
-
